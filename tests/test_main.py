d827cb172d4f45aeb16dd512ee420183c84562ca
Yufan You <ouuansteve@gmail.com>
2020-08-16 15:46:59 +0800

    feat: more accurate graph

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,18 +13,26 @@ class TestMain(unittest.TestCase):
                          "0% should return ░░░░░░░░░░░░░░░░░░░░░░░░░")
         self.assertEqual(make_graph(100), "█████████████████████████",
                          "100% should return █████████████████████████")
-        self.assertEqual(make_graph(50), "████████████░░░░░░░░░░░░░",
-                         "50% should return ████████████░░░░░░░░░░░░░")
-        self.assertEqual(make_graph(25), "██████░░░░░░░░░░░░░░░░░░░",
-                         "25% should return ██████░░░░░░░░░░░░░░░░░░░")
-        self.assertEqual(make_graph(75), "██████████████████░░░░░░░",
-                         "75% should return ██████████████████░░░░░░░")
-        self.assertEqual(make_graph(3.14), "░░░░░░░░░░░░░░░░░░░░░░░░░",
-                         "3.14% should return ░░░░░░░░░░░░░░░░░░░░░░░░░")
-        self.assertEqual(make_graph(9.901), "██░░░░░░░░░░░░░░░░░░░░░░░",
-                         "9.901% should return ██░░░░░░░░░░░░░░░░░░░░░░░")
-        self.assertEqual(make_graph(87.5), "██████████████████████░░░",
-                         "87.5% should return ██████████████████████░░░")
+        self.assertEqual(make_graph(50), "████████████▒░░░░░░░░░░░░",
+                         "50% should return ████████████▒░░░░░░░░░░░░")
+        self.assertEqual(make_graph(50.001), "████████████▓░░░░░░░░░░░░",
+                         "50.001% should return ████████████▓░░░░░░░░░░░░")
+        self.assertEqual(make_graph(25), "██████▒░░░░░░░░░░░░░░░░░░",
+                         "25% should return ██████▒░░░░░░░░░░░░░░░░░░")
+        self.assertEqual(make_graph(75), "██████████████████▓░░░░░░",
+                         "75% should return ██████████████████▓░░░░░░")
+        self.assertEqual(make_graph(3.14), "▓░░░░░░░░░░░░░░░░░░░░░░░░",
+                         "3.14% should return ▓░░░░░░░░░░░░░░░░░░░░░░░░")
+        self.assertEqual(make_graph(9.901), "██▒░░░░░░░░░░░░░░░░░░░░░░",
+                         "9.901% should return ██▒░░░░░░░░░░░░░░░░░░░░░░")
+        self.assertEqual(make_graph(87.334), "██████████████████████░░░",
+                         "87.334% should return ██████████████████████░░░")
+        self.assertEqual(make_graph(87.333), "█████████████████████▓░░░",
+                         "87.333% should return █████████████████████▓░░░")
+        self.assertEqual(make_graph(4.666), "█░░░░░░░░░░░░░░░░░░░░░░░░",
+                         "4.666% should return █░░░░░░░░░░░░░░░░░░░░░░░░")
+        self.assertEqual(make_graph(4.667), "█▒░░░░░░░░░░░░░░░░░░░░░░░",
+                         "4.667% should return █▒░░░░░░░░░░░░░░░░░░░░░░░")
 
 
 if __name__ == '__main__':
